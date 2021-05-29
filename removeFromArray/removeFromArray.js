31a6b9dc731c951c30cf252f5caeb442a8d1ee18
hellolol2016 <dennisthederp812@gmail.com>
2021-05-29 14:35:02 -0700

    did some practices

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, item, item2) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i]===item || arr[i] == item2){
+            arr.splice(i,1);
+        }
+    }
+    return arr
 };
 
 module.exports = removeFromArray;
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, rep) {
+    let str2 ="";
+    if(rep < 0){
+        return("ERROR")
+    }
+    for(let i = 0; i < rep; i++){
+        str2+=str;
+    }
+    return(str2);
 };
 
 module.exports = repeatString;
