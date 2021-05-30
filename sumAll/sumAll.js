974edcce1bb0df995fb089e257850a980a48cdae
hellolol <denniswang812@gmail.com>
2021-05-30 09:34:58 -0700

    did sumall

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if ( a < 0 || b < 0 || typeof a != "number" || typeof b != "number"){
+        return "ERROR"
+    }
+    let sum = 0;
+    if(a > b){
+        let temp = b;
+        b = a;
+        a = temp;
+    }
+    for(let i = a; i <= b; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 module.exports = sumAll;
